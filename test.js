ac69ec4347176d953a4c7c7aa8ff371bfc255739
swapnil <swapnil.gadekar@in.tesco.com>
2017-09-03 16:07:48 +0530

    updating test cases

--- test.js
+++ test.js
@@ -9,48 +9,62 @@ const cars=require('./cars');
 
 const obj = new model();
 
-describe('Unit test for booking pink cab',()=>{
-    let pink=true,lat=20,lan=20;
-    it("Should book an available pink  cab",()=>{
-        let result=obj.bookride(pink,lat,lan);
-        expect(result).to.be.a('object');
-        result.lat.should.equal(lat);
-        result.lan.should.equal(lan);
-        result.pink.should.equal(true);
+describe('Unit test cases for cab booking app',()=>{
+    describe('Unit test for booking cab',()=>{
+        it("Should book an available cab",()=>{
+            let pink=false,lat=25,lan=25;
+            let result=obj.bookride(pink,lat,lan);
+            expect(result).to.be.a('object');
+            result.id.should.equal(105);
+            result.lat.should.equal(lat);
+            result.lan.should.equal(lan);
+        });
+        it("Should book an available pink  cab",()=>{
+            let pink=true,lat=20,lan=20;
+            let result=obj.bookride(pink,lat,lan);
+            expect(result).to.be.a('object');
+            result.id.should.equal(103);
+            result.lat.should.equal(lat);
+            result.lan.should.equal(lan);
+            result.pink.should.equal(true);
+        });
+    });
+
+    describe('Unit test for ending ride',()=>{
+        let id=101,lat=50,lan=50;
+        it("Should end ride and return distance and price",()=>{
+            let result=obj.endride(id,lat,lan);
+            expect(result).to.be.a('string');
+            expect(result).to.have.string('distance');
+            expect(result).to.have.string('price');
+        });
+        it("Should update the cab location to user location",()=>{
+            let result=obj.endride(id,lat,lan);
+            let bookedCab = _.findWhere(cars,{id:id});
+            bookedCab.lat.should.equal(lat);
+            bookedCab.lan.should.equal(lan);
+            bookedCab.availabel.should.equal(true);
+        });
+        it("Should end the pink cab ride",()=>{
+            let id=103;
+            let result=obj.endride(id,lat,lan);
+            expect(result).to.be.a('string');
+            expect(result).to.have.string('distance');
+            expect(result).to.have.string('price');
+        });
+    });
+
+    describe('Unit test for cabs not available',()=>{
+        let message = 'No cabs found, please try again later';
+        it('should return no cabs available message',()=>{
+            let stub = sinon.stub(obj,'bookride');
+            stub.returns('No cabs found, please try again later');
+            let result=obj.bookride(false,25,25);
+            result.should.be.a('string');
+            expect(result).to.equal(message);
+            stub.reset();
+        });
     });
 });
 
-describe('Unit test for booking cab',()=>{
-    let pink=false,lat=10,lan=10;
-    it("Should book an available cab",()=>{
-        let result=obj.bookride(pink,lat,lan);
-        expect(result).to.be.a('object');
-        result.lat.should.equal(lat);
-        result.lan.should.equal(lan);
-    });
-});
-
-describe('Unit test for ending ride',()=>{
-    let id=101,lat=50,lan=50;
-    it("Should end ride and return distance and price",()=>{
-        let result=obj.endride(id,lat,lan);
-        expect(result).to.be.a('string');
-        expect(result).to.have.string('distance');
-        expect(result).to.have.string('price');
-    });
-    it("Should update the cab location to user location",()=>{
-        let result=obj.endride(id,lat,lan);
-        let bookedCab = _.findWhere(cars,{id:id});
-        bookedCab.lat.should.equal(lat);
-        bookedCab.lan.should.equal(lan);
-        bookedCab.availabel.should.equal(true);
-    });
-    it("Should end the pink cab ride",()=>{
-        let id=103;
-        let result=obj.endride(id,lat,lan);
-        expect(result).to.be.a('string');
-        expect(result).to.have.string('distance');
-        expect(result).to.have.string('price');
-    });
-});
 
